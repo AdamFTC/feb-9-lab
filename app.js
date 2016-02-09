73cb673ca484ead29477931aaac5baaa7e08a204
Adam Leonard <adammhleonard@gmail.com>
2016-02-09 11:04:16 -0800

    add index and js files

---
+++ app.js
@@ -0,0 +1,29 @@
+/* Have user answer 6 questions.
+Store all user answers as variables. Return those answers to user by concatenating each answer with a reply.
+console.log all user questions and answers so they may be clearly read in the dev console. */
+
+var userName = prompt('What is your name, citizen?');
+console.log('The user\'s name is ' + userName + '.');
+alert('Nice to meet you, ' + userName + '!');
+
+
+
+var userBev = prompt('What is your favorite morning beverage?');
+console.log('The user\'s preferred morning drink is ' + userBev + '.');
+alert('Wow, I wouldn\'t have guessed that ' + userBev + ' would be your preferred breakfast drink. My favorite is Gin mixed with raw coffee grounds.')
+
+var userMicro = prompt('Moving on, what is your favorite microarchitecture?');
+console.log('The user\'s favorite microarchitecture is ' + userMicro + '.');
+alert('Cool, ' + userMicro + ' is a classic!');
+
+var userMusic = prompt('Who is one of your favorite musicians or composers?');
+console.log('The user\'s favorite composer is ' + userMusic + '.');
+alert('Oh cool, I saw ' + userMusic + ' live in concert like 8 times last year.');
+
+var userPol = prompt('What is your favorite political party?');
+console.log('The user\'s party affiliation is ' + userPol + '. Put them under surveillance for possible sedicious activities.');
+alert(userPol + 'is an interesting choice! I tend to vote with the Swedish Pirate Party. Yarr!!!1one');
+
+var userBook = prompt('What is your favorite book?');
+console.log('The user\'s favorite book is ' + userBook + '.');
+alert('No way, ' + userBook + ' is my favorite too! Altered Carbon by Richard K Morgan is a close second.');
